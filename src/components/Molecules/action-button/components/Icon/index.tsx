ce469f2e605f38e01df9bc04a65be84fa75684c9
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-18 13:52:03 -0300

    feat(DDS): add isloading prop to Icon

--- src/components/Molecules/action-button/components/Icon/index.tsx
+++ src/components/Molecules/action-button/components/Icon/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { WrapperIcons } from "../../styles";
 import { IconType } from "./types";
 
-export function Icon({ icon }: IconType) {
-  return <WrapperIcons>{icon}</WrapperIcons>;
+export function Icon({ icon, isloading }: IconType) {
+  return <WrapperIcons isloading={isloading}>{icon}</WrapperIcons>;
 }
