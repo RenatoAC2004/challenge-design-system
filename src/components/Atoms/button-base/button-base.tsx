3d8289eac15ab18cc183e2772602dd65eb20a880
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-18 13:49:50 -0300

    fix(DDS): add rest parameter to button-base

--- src/components/Atoms/button-base/button-base.tsx
+++ src/components/Atoms/button-base/button-base.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { Container } from "./button-base.styles";
 import { ButtonBaseProps } from "./types";
 
-export function ButtonBase({ children, props }: ButtonBaseProps) {
+export function ButtonBase({ children, ...props }: ButtonBaseProps) {
   return <Container {...props}>{children}</Container>;
 }
