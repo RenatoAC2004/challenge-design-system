2e24959e5384e40c66e87e0a8ad7a61632295629
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-25 15:50:50 -0300

    feat(DDS): update action-button root

--- src/components/Molecules/action-button/components/Root/index.tsx
+++ src/components/Molecules/action-button/components/Root/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { ActionButtonProps } from "../../types";
 import { Container } from "../../styles";
-import { LucideCheck, LucideLoaderCircle } from "lucide-react";
-import { Icon } from "../Icon";
 
-export function Root({ label, loading, saved, ...props }: ActionButtonProps) {
+export function Root({ children, ...props }: ActionButtonProps) {
+
   return (
     <Container {...props}>
-      {(loading || saved) && (
-        <Icon icon={loading ? <LucideLoaderCircle /> : <LucideCheck />} isloading={loading} />
-      )}
-      {label}
+      {children}
     </Container>
   );
 }
