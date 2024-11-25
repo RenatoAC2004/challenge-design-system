c322e30ae2fb31328f353ee083ac0d4e0226c524
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-25 15:51:54 -0300

    feat(DDS): update alternative-button root

--- src/components/Molecules/alternative-button/components/Root/index.tsx
+++ src/components/Molecules/alternative-button/components/Root/index.tsx
@@ -1,6 +1,6 @@
 import { AlternativeButtonProps } from "../../types";
 import { Container } from "../../styles";
 
-export function Root({ label, ...props }: AlternativeButtonProps) {
-  return <Container {...props}>{label}</Container>;
+export function Root({ children, ...props }: AlternativeButtonProps) {
+  return <Container {...props}>{children}</Container>;
 }
