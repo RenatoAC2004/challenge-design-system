a24d0925008286de2ca2f740c0dc36dceab260a8
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-25 15:51:34 -0300

    feat(DDS): update action-button types

--- src/components/Molecules/action-button/types.ts
+++ src/components/Molecules/action-button/types.ts
@@ -1,7 +1,6 @@
 import { ButtonBaseProps } from "@/components/Atoms/button-base/types";
 
 export interface ActionButtonProps extends ButtonBaseProps {
-  label: string;
   loading?: boolean;
   saved?: boolean;
 }
