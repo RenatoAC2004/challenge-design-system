7d2745bbd8ae6b9689c1fc21d947c635f3c135a3
RenatoAC2004 <108144847+RenatoAC2004@users.noreply.github.com>
2024-11-26 15:31:12 -0300

    feat(DDS): create default-label styles

---
+++ src/components/Molecules/labels/default-label/styles.ts
@@ -0,0 +1,12 @@
+import { LabelBase } from "@/components/Atoms/label-base";
+import styled from "styled-components";
+
+export const Container = styled(LabelBase)``;
+
+export const WrapperIcons = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 4px;
+  width: 16px;
+  height: 16px;
+`;
